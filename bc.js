14a93de57853b3bd45c33826eeaac895d8794a77
TofekHamodii <43014320+TofekHamodii@users.noreply.github.com>
2018-10-24 22:33:00 +0300

    Create bc.js

---
+++ bc.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag} !`);
+          client.user.setActivity(",Ritual' Road To, 1k.",{type: 'WATCHING'});
+  
+  });
+  
+client.login(process.env.BOT_TOKEN);
